61fb61155e39c702fdf6e8c5d2b2389980689b0d
adarsh1016 <adarshjaiswal1016@gmail.com>
2025-08-27 17:32:33 +0530

    build the user feed and profile edit feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,9 @@ const navBar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑‍💻DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑‍💻DevTinder
+        </Link>
       </div>
       {user && (
         <div className="flex gap-2">
@@ -37,7 +39,7 @@ const navBar = () => {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img alt="user photo" src={user.photoURL} />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -45,10 +47,10 @@ const navBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
